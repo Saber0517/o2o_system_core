20fd9a6f4d9aaeb7c688a72d66125dad1b29da49
ZHANGJA4 <JAMES.ZHANG@OOCL.COM>
2015-09-10 13:11:28 +0800

    rename package

--- src/main/java/com/oocl/jyhon/dao/PackageEntityDao.java
+++
@@ -1,9 +0,0 @@
-package com.oocl.jyhon.dao;
-
-import com.oocl.jyhon.entiy.PackageEntity;
-
-/**
- * Created by ZHANGJA4 on 8/6/2015.
- */
-public interface PackageEntityDao extends EntityDao<PackageEntity> {
-}
